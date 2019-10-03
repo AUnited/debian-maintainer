ff07e2f69a6a51738e7d831d79075fbccd65c172
dragonzx <dragonzx@yandex.ru>
2019-10-03 06:33:10 -0700

    initial commit

---
+++ glpi/backup/filesbackup.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+# It runs once per month, no rsync and cleanup (main script do it daily)
+# Tar-ed to *.xz, so it is not displayed at GLPI backup/restore feature
+BASE="/usr/share/glpi" # GLPI script location
+DBVER="9.4.2" # GLPI version
+DATESTAMP=$(date +%Y-%m-%d)
+
+# Dump files GLPI
+tar cvfJ "${BASE}"/files/_dumps/glpi-"${DBVER}"-"${DATESTAMP}"-files.tar.xz --exclude="_*" "${BASE}"/files/
+tar cvfJ "${BASE}"/files/_dumps/glpi-"${DBVER}"-"${DATESTAMP}"-plugins.tar.xz "${BASE}"/plugins/
---
+++ glpi/backup/sqlbackup.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+user=""       # Database user
+db=""          # Database name
+mdp=""        # Databaser password
+BASE="/usr/share/glpi" # GLPI script location
+DBVER="9.4.4"          # GLPI version
+DATESTAMP=$(date +%Y-%m-%d)
+# Backup server
+buser="" # Backup server user
+bsite="" # Backup server name/ip
+bdir="/share/MD0_DATA/Backups/glpi" # Backup folder
+
+# Dump bases GLPI
+mysqldump -u"${user}" -p"${mdp}" --single-transaction "${db}" | gzip > "${BASE}"/files/_dumps/glpi-"${DBVER}"-"${DATESTAMP}".sql.gz
+nice -n +17 rsync -av "${BASE}"/files/_dumps/ "${buser}"@"${bsite}":"${bdir}"
+find "${BASE}"/files/_dumps/  -type f -mtime +91 -exec rm {} \;
