3d809b9ff79a8d4e9d6d880734baaab4a1ad7d3c
dragonzx <dragonzx@yandex.ru>
2019-10-17 06:50:51 -0700

    Fix structure

--- glpi/makepkg.sh
+++ glpi/makepkg.sh
@@ -1,2 +1,22 @@
 #!/usr/bin/env bash
 # Used for making a debian package as is
+Do you want to install package-making tools?
+apt install dpkg debconf debhelper lintian
+# Tuning up version and destination
+pkgname="glpi"
+version="9.4.4"
+arch="all"
+spath=$(pwd)
+# Here we are downloading and untaring archive
+mkdir /tmp/"${pkgname}"
+cd /tmp/"${pkgname}"
+mkdir source
+mkdir package
+cd source
+wget -i https://github.com/glpi-project/glpi/releases/download/"${version}"/glpi-"${version}".tgz
+tar xvfz glpi-"${version}".tgz && rm rf glpi-"${version}".tgz
+cp "${spath}" /tmp/"${pkgname}"/package
+# Fixing source to the package structure
+cp . ../package/usr/share/glpi
+cd ../package
+mkdir
