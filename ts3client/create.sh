a70a3a1f9d7e9165060dcdf342330462fa8c08a4
DragonZX <DragonZX@yandex.ru>
2020-04-19 19:43:29 +0300

    Createscripts ts3

---
+++ ts3client/create.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+VERSION="3.5.2"
+wget https://files.teamspeak-services.com/releases/client/$VERSION/TeamSpeak3-Client-linux_amd64-$VERSION.run
+./TeamSpeak3-Client-linux_amd64-$VERSION.run
+mv TeamSpeak3-Client-linux_amd64-$VERSION opt/ts3client
+rm -f /opt/ts3client/ts3client_runscript.sh
